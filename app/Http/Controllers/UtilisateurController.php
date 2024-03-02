00fd8f0ab7ac53cd41c377fd75c9f3d5c9116536
ninohantonio <ninohantonio@gmail.com>
2024-03-02 16:41:39 +0300

    store user commit

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Utilisateur;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Str;
 
 class UtilisateurController extends Controller
 {
@@ -21,6 +23,113 @@ class UtilisateurController extends Controller
     public function store(Request $request)
     {
         //
+
+         $userid = '';
+           if($request->input('id')){
+                $userid = $request->input('id');
+
+           }
+           else {$userid = Str::random(10) . time();}
+
+       if($request->file('profile') != null)
+       {
+           $request->validate([
+               'file' => 'required|image|mimes:png,jpg,jpeg,gif'
+           ]);
+
+           try
+           {
+               $file = $request->file('profile');
+               $filename = 'pdp_'.$request->input('userName').'.'.$file->getClientOriginalExtension();
+               $path = $file->storeAs('images', $filename, 'public');
+
+               $loginuser =  Utilisateur::firstOrCreate(
+                   ['email' => $request->input('email')],
+                   [
+                   'id' => $userid,
+                   'username' => $request->input('username'),
+                   'firstName' => $request->input('firstName'),
+                   'lastName' => $request->input('lastName'),
+                   'email' => $request->input('email'),
+                   'profileUrl' => $path
+                    ]);
+
+                   //  dd($loginuser->mail);
+
+
+                   if($loginuser){
+                       Auth::login($loginuser);
+
+                       $user = Auth::user();
+
+                       // dd($user->userId);
+
+                       $token = JWTAuth::fromUser($loginuser, ['userid'=>$loginuser->id]);
+                       $loginuser->forceFill(['api_token' => $token])->save();
+                       $cookie = cookie('jwt', $token, 60 * 24);
+
+                       // dd(Auth::user());
+                       return response([
+                           'message' => "Succes",
+                           'token' => $token,
+                       ], 200)->withCookie($cookie);
+                   }
+                   else{
+                       return response()->json(["message" => "Error creating"], 401);
+                   }
+           }
+           catch (\Exception $th)
+           {
+               return response()->json(["created"=>false, "errorMessage"=>$th->getMessage()], 500);
+           }
+       }
+
+       else {
+           try
+           {
+               // dd($request->all());
+               $loginuser =  Utilisateur::firstOrCreate(
+                                 ['email' => $request->input('email')],
+                                 [
+                                 'id' => $userid,
+                                 'username' => $request->input('username'),
+                                 'firstName' => $request->input('firstName'),
+                                 'lastName' => $request->input('lastName'),
+                                 'email' => $request->input('email'),
+
+                          ]);
+
+               //  dd($loginuser->mail);
+
+
+               if($loginuser){
+                   Auth::login($loginuser);
+
+                   $user = Auth::user();
+
+                   // dd($user->userId);
+
+                    $token = JWTAuth::fromUser($loginuser, ['userid'=>$loginuser->id]);
+                   $loginuser->forceFill(['api_token' => $token])->save();
+                   $cookie = cookie('jwt', $token, 60 * 24);
+
+                   // dd(Auth::user());
+                   return response([
+                       'message' => "Succes",
+                       'token' => $token
+                   ], 200)->withCookie($cookie);
+               }
+               else{
+                   return response()->json(["message" => "Error creating"], 401);
+               }
+
+           }
+           catch (\Exception $th)
+           {
+               return response()->json(["created"=>false, "errorMessage"=>$th->getMessage()], 500);
+           }
+       }
+
     }
 
     /**
